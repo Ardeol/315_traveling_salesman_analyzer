df63bc83ec754ad1c4ceb261a7b1b0c1d344777e
Ardeol <new0programmer@gmail.com>
2015-01-26 23:08:43 -0600

    Auto-scale the Graph
    Adds some code which auto-scales the output graph in the event that input points are not in the given 1000x1000 window.  I have never used Python before, so I apologize if the way I did it is kind of crude.

--- analyzer.py
+++ analyzer.py
@@ -15,21 +15,40 @@ for line in f:
     numbers = line.split();
     x = int(numbers[0])
     y = int(numbers[1])
-    pt = Point( x, y )
-    pt.draw(win)
+    #pt = Point( x, y )
+    #pt.draw(win)
     given_point_order += [x,y]
 
 f.close()
 f = open('generated_output.txt', 'r')
 prev_x = -1
 prev_y = -1
+
+# Scale Graph
+padding = 10
+xs = given_point_order[::2]
+ys = given_point_order[1::2]
+minX = min(xs)
+minY = min(ys)
+xScale = (1000.0 - 2.0*padding) / (max(xs) - minX)
+yScale = (1000.0 - 2.0*padding) / (max(ys) - minY)
+xTrans = -minX * xScale + padding
+yTrans = -minY * yScale + padding
+# End Scale Info
+# Remove Scaling:
+# xScale = 1
+# yScale = 1
+# xTrans = 0
+# yTrans = 0
+
 for line in f:
     numbers = line.split();
     pt_no = int(numbers[0]) * 2
     x = given_point_order[pt_no]
     y = given_point_order[pt_no+1]
+    Point(x*xScale + xTrans, y*yScale + yTrans).draw(win)
     if prev_x >= 0:
-        connection = Line(Point(prev_x,prev_y), Point(x, y))
+        connection = Line(Point(prev_x*xScale + xTrans, prev_y*yScale + yTrans), Point(x*xScale + xTrans, y*yScale + yTrans))
         connection.draw(win)
         #if you want to see the graph traverse more slowly, uncomment this!
         # time.sleep(1)
